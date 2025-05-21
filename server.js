54f5f4fdef0a319afd75c27534bb747a1373476d
marbar6 <inti.boruah5@gmail.com>
2025-05-21 21:59:07 +0530

    Add server

---
+++ server.js
@@ -0,0 +1,17 @@
+// let express=require("express");
+// const path = require("path");
+// let PORT = 9000;
+// let router=express();
+
+
+// router.use(express.static(path.join(__dirname,"portfolio")))
+
+
+// // now this is the all routes
+// router.get("/",(req,res)=>{
+//     res.sendFile(path.join("D:/project/portfolio","index.html"));
+// })
+
+// router.listen(PORT,()=>{
+//     console.log(`The port is listen in ${PORT}`);
+// })
